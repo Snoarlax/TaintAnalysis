a8f10e5c8edf3c3d4f25641e2c29338e7c4be43b
Snoarlax <shahnoorkiani14@gmail.com>
2022-04-04 04:56:44 -0700

    Began making the taint messsage more verbose.

--- src/Statement/TaintType.java
+++ src/Statement/TaintType.java
@@ -1,9 +1,30 @@
 package Statement;
 
 public enum TaintType {
-    Default,
-    SQLI, // can do " or '
-    XSS, // can do <, >
-    INJECTION, // can do ;, ||, &&
-    DIRECTORY // can do .. , /
+    // todo: replace with constants
+    Default("For Debugging purposes. ", "", "", ""),
+    SQLI("SQL Injection", "HIGH", "HIGH", "HIGH"), // can do " or '
+    XSS("Cross site scripting", "low", "low", "low"), // can do <, >
+    INJECTION("Code injection", "HIGH", "HIGH", "HIGH"), // can do ;, ||, &&
+    DIRECTORY("Directory Traversal", "HIGH", "HIGH", "low"); // can do .. , /
+
+
+    private final String Message, Confidentiality, Integrity, Availability;
+
+    TaintType(String message, String confidentiality, String integrity, String availability) {
+        Message = message;
+        Confidentiality = confidentiality;
+        Integrity = integrity;
+        Availability = availability;
+    }
+
+
+    public String generateMessage() {
+        int spacing = Integer.max(15, Message.length());
+        String header = String.format("%"+spacing+"s" + "%"+spacing+"s" + "%"+spacing+"s" + "%"+spacing+"s", "Vulnerability", "Confidentiality", "Integrity", "Availability");
+        String content = String.format("%"+spacing+"s" + "%"+spacing+"s" + "%"+spacing+"s" + "%"+spacing+"s", Message,Confidentiality,Integrity,Availability);
+
+        return String.format("%s\n%s",header,content);
+    }
+
 }
--- src/TaintAnalyser.java
+++ src/TaintAnalyser.java
@@ -1,9 +1,12 @@
+import Statement.Statement;
 import Statement.Variable;
 
+import javax.swing.plaf.nimbus.State;
 import java.util.*;
 
 public class TaintAnalyser {
     // mark successors as changed, so they recompute the tainted set
+    // todo: use constants for the output
     public static void main(String[] args) throws InvalidFileException {
         CFGParser parser = new CFGParser(args[0]);
         // Use a FiFo queue to manage which blocks need to be analysed.
@@ -19,7 +22,35 @@ public class TaintAnalyser {
         }
 
         for (Block block : parser.getBlocks())
-            if (block.isTaintedSink())
+            if (block.isTaintedSink()) {
                 System.out.println("Block: " + block.getBlockName() + " Is Tainted! ");
+                System.out.println();
+                System.out.println("TAINT CHAIN:");
+                for (Statement statement : block.getStatements())
+                    if (statement.isTaintedSink()) {
+                        HashSet<Variable> TaintedFrom = statement.TaintedBy();
+                        ArrayList<String> TaintChain = new ArrayList<>();
+
+                        // todo: put the sink name at the start of the chain
+                        while (!TaintedFrom.isEmpty()) {
+                            // constructs each link in the chain of variables which caused the taint.
+                            StringBuilder CurrentVariableLink = new StringBuilder("[");
+                            for (Variable variable : TaintedFrom)
+                                CurrentVariableLink.append(String.format(" %s", variable.getVariableName()));
+                            CurrentVariableLink.append(" ]");
+                            TaintChain.add(CurrentVariableLink.toString());
+
+                            // finds the union of the variables that tainted these variables
+                            HashSet<Variable> NewTaintedFrom = new HashSet<>();
+                            for (Variable variable : TaintedFrom) {
+                                NewTaintedFrom.addAll(variable.getTaintedFrom());
+                            }
+                            TaintedFrom = NewTaintedFrom;
+                        }
+
+                        System.out.println("        " + String.join(" --> ",  TaintChain));
+                        System.out.println();
+                    }
+            }
     }
 }
